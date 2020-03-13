cc7449a8da9d891108e6be4d2bfa18ce3818b0fc
Samuel Olagesin <feyisamuell@gmail.com>
2020-03-13 13:40:08 +0100

    Initial Commit

---
+++ script1.py
@@ -0,0 +1,17 @@
+# importing the various packages/libraries needed for the program
+import folium as fl
+import pandas as pd
+
+
+volcanoes_data = pd.read_csv("Volcanoes-USA.txt")
+
+map = fl.Map(location = [48.082, -121.6233], zoom_start = 4, tiles = 'Stamen Terrain')
+
+
+for latitude,longitude,name in zip(volcanoes_data["LAT"], volcanoes_data["LON"], volcanoes_data["NAME"]):
+    map.add_child(fl.Marker(location=[latitude, longitude], popup=name, color = "red"))
+
+
+
+
+map.save(outfile = 'test.html')
