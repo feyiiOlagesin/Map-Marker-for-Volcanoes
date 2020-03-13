44cea5bac83635a24d80dc49017bf30299f2b98e
Samuel Olagesin <feyisamuell@gmail.com>
2020-03-13 13:52:57 +0100

    added comments to the script1.py file

--- script1.py
+++ script1.py
@@ -2,12 +2,14 @@
 import folium as fl
 import pandas as pd
 
-
+#read the volcanoe data from the txt file using pandas
 volcanoes_data = pd.read_csv("Volcanoes-USA.txt")
 
-map = fl.Map(location = [48.082, -121.623 3], zoom_start = 4, tiles = 'Stamen Terrain')
+#create an object of the Map
+map = fl.Map(location = [48.082, -121.6233], zoom_start = 4, tiles = 'Stamen Terrain')
 
 
+#loop through and assign the values in the data set to the object of map and assign markers
 for latitude,longitude,name in zip(volcanoes_data["LAT"], volcanoes_data["LON"], volcanoes_data["NAME"]):
     map.add_child(fl.Marker(location=[latitude, longitude], popup=name, color = "red"))
 
