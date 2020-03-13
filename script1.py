e9ebf7356803a7b9c1c6b2dc19032fce759aa26f
Samuel Olagesin <feyisamuell@gmail.com>
2020-03-13 13:47:18 +0100

    another change

--- script1.py
+++ script1.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 volcanoes_data = pd.read_csv("Volcanoes-USA.txt")
 
-map = fl.Map(location = [48.082, -121.6233], zoom_start = 4, tiles = 'Stamen Terrain')
+map = fl.Map(location = [48.082, -121.623 3], zoom_start = 4, tiles = 'Stamen Terrain')
 
 
 for latitude,longitude,name in zip(volcanoes_data["LAT"], volcanoes_data["LON"], volcanoes_data["NAME"]):
