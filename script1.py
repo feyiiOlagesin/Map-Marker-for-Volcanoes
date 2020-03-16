b097013e22afdc7ab28968d36a6aaafa546a8373
Samuel Olagesin <feyisamuell@gmail.com>
2020-03-16 22:18:29 +0100

    new changes to marker colors

--- script1.py
+++ script1.py
@@ -9,10 +9,18 @@ volcanoes_data = pd.read_csv("Volcanoes-USA.txt")
 #This also signifies the starting position of the map when loaded
 map = fl.Map(location = [48.082, -121.6233], zoom_start = 4, tiles = 'Stamen Terrain')
 
+def colorFinder(elev):
+    if elev in range(0, 1000):
+        color = "green"
+    elif elev in range(1000, 3000):
+        color = "orange"
+    else:
+        color = "red"
+    return color
 
 #loop through and assign the values in the data set to the object of map and assign markers
-for latitude,longitude,name in zip(volcanoes_data["LAT"], volcanoes_data["LON"], volcanoes_data["NAME"]):
-    map.add_child(fl.Marker(location=[latitude, longitude], popup=name, color = "red"))
+for latitude,longitude,name,elevation in zip(volcanoes_data["LAT"], volcanoes_data["LON"], volcanoes_data["NAME"], volcanoes_data["ELEV"]):
+    map.add_child(fl.Marker(location=[latitude, longitude], popup=name,color = colorFinder(elevation)))
 
 
 
